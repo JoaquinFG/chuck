6c8ee044b6e242ef09bb1f54edb8c27b446ad03d
Joaquin_FG <48914838+JoaquinFG@users.noreply.github.com>
2021-06-07 17:48:51 +0100

    evento handlerJoke muestra frase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Chuck Norris</h1>
         <Categorias />
       </div>
--- src/components/Categorias.js
+++ src/components/Categorias.js
@@ -2,31 +2,57 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class Categorias extends Component {
-  state = {
-    category: [],
-  };
+  constructor() {
+    super();
+    this.state = {
+      category: [],
+      joke: "",
+    };
+    this.handlerJoke = this.handlerJoke.bind(this);
+  }
 
   componentDidMount() {
     axios
       .get("https://api.chucknorris.io/jokes/categories")
       .then((res) => {
-        console.log(res);
         this.setState({ category: res.data });
       })
       .catch((err) => console.log(err));
   }
 
+  handlerJoke = function (e) {
+    const opcion = e.target.value;
+
+    axios
+      .get("https://api.chucknorris.io/jokes/random?category=" + opcion)
+      .then((res1) => {
+        this.setState({ joke: res1.data.value });
+      })
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
       <div className="jumbotron">
-        <h2>Selecciona una categoría</h2>
-        <select>
-          {this.state.category.map((elemento) => (
-            <option key={elemento} value={elemento}>
-              {elemento}
-            </option>
-          ))}
-        </select>
+        <div className="row">
+          <div className="col-3">
+            <h2>Categorías</h2>
+            <select className="mt-3" onChange={this.handlerJoke}>
+              <option value={-1}>Selecciona una categoría:</option>
+              {this.state.category.map((elemento) => (
+                <option key={elemento} value={elemento}>
+                  {elemento}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="col-9">
+            <h2>Frase</h2>
+            <div className="mt-3">
+              <p>{this.state.joke}</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
